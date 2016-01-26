04a94d431569ac3a65a5f1132e89e975f406477a
CalvinKeum <Calvin.Keum@liferay.com>
2016-01-26 12:06:51 -0800

    Adding login feature

--- liferay-bathroom-jamz.js
+++ liferay-bathroom-jamz.js
@@ -1,23 +1,24 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+	//Login Templates -- DONT REMOVE
+	Template.body.helpers({
+		firstName: function(){
+			var user = Meteor.user(); 
+			if (user) {
+				return user.services.google.given_name;    
+			} 
+		},
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+		profileURL: function() {
+			var user = Meteor.user(); 
+			if (user) {
+				return user.services.google.picture; 
+			} 
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 }
