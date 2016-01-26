35f4060dd4706cfe18562d3c9d7af7f7e12f875e
Weston Hancock <weston.hancock@liferay.com>
2016-01-26 15:24:45 -0800

    add data to collection and display with templates

--- app.js
+++ app.js
@@ -1,87 +1,72 @@
-
-// updateGenre = function(old, new) {
-
-// }
-
-
 Music = new Mongo.Collection("music");
-CurrentSelection = new Mongo.Collection("current");
-// Music.remove({});
 
-  CurrentSelection.insert({
-  	old : "",
-  	newSelection : ""
-  })
+if (Meteor.isServer) {
+	Meteor.startup(function () {
+		var musicObjects = [
+			{
+				description : "Roll down your windows while driving your pickup",
+				id : 0,
+				name : "Country",
+				votes : 4
+			},
+			{
+				description : "",
+				id : 0,
+				name : "K-Pop",
+				votes : 0
+			},
+			{
+				description : "",
+				id : 0,
+				name : "Disney",
+				votes : 5
+			},
+			{
+				description: "",
+				id : 4,
+				name : "Techno",
+				votes : 0
+			},
+			{
+				description: "",
+				id : 4,
+				name : "Metal",
+				votes : 2
+			},
+			{
+				description: "",
+				id : 4,
+				name : "Filmscores",
+				votes : 0
+			}
+		];
 
-  Music.insert({
-  	country : {
-  		id : 0,
-  		votes : 0,
-  		description : "Roll down your windows while driving your pickup"
-  	},
-  	kpop : {
-  		id : 0,
-  		votes : 0,
-  		description : ""
-  	},
-  	disney : {
-  		id : 0,
-  		votes : 0,
-  		description : ""
-  	},
-  	techno: {
-  		id : 4,
-  		votes : 0,
-  		description: ""
-  	},
-  	metal: {
-  		id : 4,
-  		votes : 0,
-  		description: ""
-  	},
-  	soundtrack: {
-  		id : 4,
-  		votes : 0,
-  		description: ""
-  	}
-
-
-  })
-
+		if (Music.length == 0) {
+			for (var i = 0; i < musicObjects.length; i++) {
+				Music.insert(musicObjects[i])
+			}
+		}
+	});
+}
 
 
 if (Meteor.isClient) {
-
-  
-
-
-
-Template.nextUp.helpers({
-	score: function () {
-
-      return Music.find();
-    }
-})
+	Template.bodyTemplate.helpers({
+		score: function () {
+				return Music.find({}, {sort: { votes: -1 }});
+			}
+	});
 
 
 
+	Template.bodyTemplate.events({
+		'change #genreList' : function(evt) {
+			console.log("changed!");
 
-  Template.hello.events({
-    'change #genreList' : function(evt) {
-    	console.log("changed!");
+			var selected = $("input[name=genre]:checked").val();
 
-    	var selected = $("input[name=genre]:checked").val();
-
-    	CurrentSelection.update(old)
-    	console.log(selected);
-
-   
-  		
-    }
-  })
-
-  
-
-
-
+			CurrentSelection.update(old);
+			console.log(selected);
+		}
+	});
 }
