9358a36edef13e3312dc39bdda5f8871c23cbaac
Weston Hancock <weston.hancock@liferay.com>
2016-01-26 17:05:00 -0800

    inc votes

--- app.js
+++ app.js
@@ -5,37 +5,37 @@ if (Meteor.isServer) {
 		var musicObjects = [
 			{
 				description : "Roll down your windows while driving your pickup",
-				id : "PLv9bNxDawPe2N33iKGQZQZE9rmg3xObxo",
+				playerId : "PLv9bNxDawPe2N33iKGQZQZE9rmg3xObxo",
 				name : "Country",
 				votes : 0
 			},
 			{
 				description : "",
-				id : "PLv9bNxDawPe1POEqcjGgrS726XslzG1BX",
+				playerId : "PLv9bNxDawPe1POEqcjGgrS726XslzG1BX",
 				name : "K-Pop",
 				votes : 0
 			},
 			{
 				description : "",
-				id : "PLv9bNxDawPe0hS_qQIFm0T97KQZXUBKQR",
+				playerId : "PLv9bNxDawPe0hS_qQIFm0T97KQZXUBKQR",
 				name : "Disney",
 				votes : 0
 			},
 			{
 				description: "",
-				id : "PLv9bNxDawPe3nU7E1FR7RFL2mXWA7gj28",
+				playerId : "PLv9bNxDawPe3nU7E1FR7RFL2mXWA7gj28",
 				name : "Techno",
 				votes : 0
 			},
 			{
 				description: "",
-				id : "PLv9bNxDawPe0ECBZ-2rkFpBjMsi_6QtUp",
+				playerId : "PLv9bNxDawPe0ECBZ-2rkFpBjMsi_6QtUp",
 				name : "Metal",
 				votes : 0
 			},
 			{
 				description: "",
-				id : "PLv9bNxDawPe1c-HYINpPqe6MDOKltJ4ze",
+				playerId : "PLv9bNxDawPe1c-HYINpPqe6MDOKltJ4ze",
 				name : "Filmscores",
 				votes : 0
 			}
@@ -53,17 +53,43 @@ if (Meteor.isServer) {
 
 
 if (Meteor.isClient) {
+	function updateVotes(newId, oldId) {
+		var music = Music.find();
+
+		music.forEach(function(item) {
+			var playerId = item.playerId;
+			var id = item._id;
+
+			if (playerId === oldId) {
+				console.log(oldId);
+				Music.update(id, {
+					$inc: {votes: -1}
+				});
+			}
+			else if (playerId === newId) {
+				console.log(newId);
+				Music.update(id, {
+					$inc: {votes: 1}
+				});
+			}
+		});
+	}
+
 	Template.bodyTemplate.helpers({
 		score: function () {
-			return Music.find({}, {sort: { votes: -1 }});
+			return Music.find(
+				{},
+				{
+					sort: { votes: -1 }
+				}
+			);
 		}
 	});
 
 	Template.bodyTemplate.events({
 		'change #genreList' : function(evt) {
-			console.log("changed!");
-
 			var selected = $("input[name=genre]:checked").val();
+
 		}
 	});
 }
